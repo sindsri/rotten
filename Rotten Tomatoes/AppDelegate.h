d44ecea8a07ee7cf34a420d6e8bb7c2dae6aac31
sindhuja sridharan <sindhusridhar05@sindhujas-MacBook-Air.local>
2014-09-17 19:13:21 -0700

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by sindhuja sridharan on 9/17/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
